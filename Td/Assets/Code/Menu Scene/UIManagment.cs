9ef362e0b4506ade49e2b5fed28c66d04d1f42ff
DwainPool <125881435+DwainPool@users.noreply.github.com>
2023-06-19 16:15:32 +0300

    Commit to UIManagement
    Положено начало методу выбора уровней, начата разработка дизайна выбора уровня и начата разработка дизайна настроек

---
+++ Td/Assets/Code/Menu Scene/UIManagment.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+public class UIManagment : MonoBehaviour
+{
+    public int level;
+   public void GoToLevelSelection()
+    {
+        SceneManager.LoadScene("LevelSelection");
+    }
+    public void GoOut()
+    {
+        Application.Quit();
+    }
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    public void GoToPlay()
+    {
+        SceneManager.LoadScene("Level " + level.ToString());
+    }
+}
